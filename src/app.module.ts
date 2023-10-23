21acd250c497a4b0b8cb452560554ef7071007c6
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-10-23 23:41:54 +0900

    configService와 .env로 쿠키 키 관리하기

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,9 +43,12 @@ const cookieSession = require('cookie-session');
 // app.enableCors({ credentials: true, origin: true });
 
 export class AppModule {
+  constructor(
+    private configService: ConfigService 
+  ) {}
   configure(consumer:MiddlewareConsumer) {
     consumer.apply(
-      cookieSession({ keys: ['supercookie'] }),
+      cookieSession({ keys: this.configService.get('COOKIE_KEY') }),
     ).forRoutes('*');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-const cookieSession = require('cookie-session');
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['supercookie']
-  }));
+  
 
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true,
